c3cb2d21f8eee3e7593727bcebc57a42ce53bd3e
vartika.k <vartika.k@LP-5CD7197KML.HCLT.CORP.HCL.IN>
2020-01-06 16:17:12 +0530

    Added Selenium code to App Class

--- src/main/java/com/vogella/build/maven/java/App.java
+++ src/main/java/com/vogella/build/maven/java/App.java
@@ -1,13 +1,27 @@
 package com.vogella.build.maven.java;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.io.File;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
+
         System.out.println( "Hello World!" );
+
+        System.out.println("Starting Testing------");
+        File file = new File(System.getProperty("user.dir")+"/src/test/resources/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.google.com");
+        System.out.println("Completed Testing------");
+
     }
 }
