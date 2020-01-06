66f12ade1db249d9695e4ba445e27b60f79a0d09
vartika.k <vartika.k@LP-5CD7197KML.HCLT.CORP.HCL.IN>
2020-01-06 17:55:03 +0530

    Adding

--- src/main/java/com/vogella/build/maven/java/App.java
+++ src/main/java/com/vogella/build/maven/java/App.java
@@ -11,7 +11,7 @@ import java.io.File;
  */
 public class App
 {
-    public static void main( String[] args )
+    public static void main( String[] args )throws Exception
     {
 
         System.out.println( "Hello World!" );
