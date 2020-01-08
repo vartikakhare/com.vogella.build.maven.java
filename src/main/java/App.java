e958ac2dfacee91724998086b265e5f0fe52fbba
vartika.k <vartika.k@LP-5CD7197KML.HCLT.CORP.HCL.IN>
2020-01-08 17:05:47 +0530

    update

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,7 +12,7 @@ public class App
     public static void main( String[] args )throws Exception
     {
 
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello World! from App" );
 
         /*System.out.println("Starting Testing------");
         File file = new File(System.getProperty("user.dir")+"/src/java/resources/chromedriver.exe");
