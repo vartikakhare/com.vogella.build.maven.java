81c65c673a90592b81e43f9aa37a87d2f37ba706
vartika.k <vartika.k@LP-5CD7197KML.HCLT.CORP.HCL.IN>
2020-01-08 16:48:06 +0530

    updates with TestNG

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,38 +1,17 @@
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.testng.annotations.Test;
 
 /**
  * Unit java for simple App.
  */
 public class AppTest 
-    extends TestCase
 {
     /**
      * Create the java case
      *
      * @param testName name of the java case
      */
-    public AppTest( String testName )
-    {
-
-        super( testName );
-        System.out.println("HappyNewYear");
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@Test 
+	public static void dummy(){
+		System.out.println("Hello from App Test");
+	}
 }
--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,18 +1,16 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
-import java.io.File;
+import org.testng.annotations.Test;
 
 public class TestOne {
 
-    public static void main(String[] args)throws Exception{
+	@Test
+    public static void setup(){
 
         //WebDriverManager.chromedriver().setup();
         System.out.println("Starting Testing------");
-        File file = new File(System.getProperty("user.dir")+"/src/java/resources/chromedriver.exe");
+       /* File file = new File(System.getProperty("user.dir")+"/src/java/resources/chromedriver.exe");
         System.setProperty("webdriver.chrome.driver", file.getAbsolutePath());
         WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com");
+        driver.get("https://www.google.com");*/
         System.out.println("Completed Testing------");
     }
 }
